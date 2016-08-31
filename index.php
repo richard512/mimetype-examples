b7cd4b037540774d4761e4639159d9e6897073ca
richard512 <richardt512@yahoo.com>
2016-08-31 02:13:28 -0700

    updated index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
 echo '<table>'."\n";
-$dirlist = scandir('.');
+$dirpath = 'filetypes/';
+$dirlist = scandir($dirpath);
 foreach ($dirlist as $file) {
 	if ($file=='.' || $file=='..') continue;
-	if (is_dir($file)) continue;
-	echo '<tr><td>'.$file.'</td><td>'.getMimeType($file).'</td></td>'."\n";
+	if (is_dir($dirpath.$file)) continue;
+	echo '<tr>';
+    echo '<td>'.$file.'</td>';
+    echo '<td>'.getMimeType($dirpath.$file).'</td>';
+    echo '</td>'."\n";
 }
 echo '</table>';
 
@@ -14,17 +18,17 @@ function getMimeType($tmpname)
 {
     $mimetype = '';
     if (function_exists('finfo_fopen')) {
-    	echo 'using finfo_fopen';
+    	//echo 'using finfo_fopen';
         $finfo = new finfo();
         $mimetype = $finfo->file($tmpname,FILEINFO_MIME_TYPE);
     } elseif (function_exists('mime_content_type')) {
-    	echo 'using mime_content_type';
+    	//echo 'using mime_content_type';
         $mimetype = mime_content_type($tmpname);
     } elseif (function_exists('getimagesize')) {
-    	echo 'using getimagesize';
+    	//echo 'using getimagesize';
         $mimetype = getimagesize($tmpname);
     } elseif (function_exists('exif_imagetype')) {
-    	echo 'using exif_imagetype';
+    	//echo 'using exif_imagetype';
         $mimetype = exif_imagetype($tmpname);
     }
     return $mimetype;
