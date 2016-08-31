2419556db94fa2afa3a3a12b8b5cefab0044a6ab
richard512 <richardt512@yahoo.com>
2016-08-31 02:58:10 -0700

    added html5 elements

--- index.php
+++ index.php
@@ -1,18 +1,60 @@
 <?php
 
-echo '<table>'."\n";
 $dirpath = 'filetypes/';
 $dirlist = scandir($dirpath);
+$filenames = array();
+$mimetypes = array();
 foreach ($dirlist as $file) {
 	if ($file=='.' || $file=='..') continue;
 	if (is_dir($dirpath.$file)) continue;
-	echo '<tr>';
-    echo '<td>'.$file.'</td>';
-    echo '<td>'.getMimeType($dirpath.$file).'</td>';
+    $filenames[] = $file;
+    $mimetypes[] = getMimeType($dirpath.$file);
+}
+
+array_multisort($mimetypes, $filenames);
+
+echo '<h3>The sample files, sorted by MIME Type</h3>';
+echo '<table>'."\n";
+echo '<thead>';
+echo '<th>File</th>';
+echo '<th>MIME Type</th>';
+echo '<th>Action</th>';
+echo '</thead>';
+echo '<tbody>' . "\n";
+foreach ($filenames as $i => $file) {
+    echo '<tr>';
+    echo '<td>';
+    echo '<a href="filetypes/'.$filenames[$i].'">';
+    echo $filenames[$i];
+    echo '</a>';
+    echo '</td>';
+    echo '<td>'.$mimetypes[$i].'</td>';
+    echo '<td>'.makeHtml5Element('filetypes/'.$filenames[$i], $mimetypes[$i]).'</td>';
     echo '</td>'."\n";
 }
+echo '</tbody>';
 echo '</table>';
 
+function makeHtml5Element($filepath, $mimetype) {
+    $arrmime = explode('/', $mimetype);
+    if ($arrmime) {
+        $mimeTopLevel = $arrmime[0];
+        switch ($mimeTopLevel) {
+            case 'application':
+                return '<a href="'.$filepath.'">Download</a>';
+                break;
+            case 'audio':
+                return '<audio controls src="'.$filepath.'"></audio>';
+                break;
+            case 'image':
+                return '<div style="height:100px;width:100px;background:url('.$filepath.');background-size: contain;background-repeat: no-repeat;"></div>';
+                break;
+            case 'video':
+                return '<video controls height="300px" width="400px" src="'.$filepath.'"></video>';
+                break;
+        }
+    }
+}
 
 function getMimeType($tmpname)
 {
@@ -35,3 +77,14 @@ function getMimeType($tmpname)
 }
 
 ?>
+
+<style type="text/css">
+    table {
+        border-collapse: collapse;
+        text-align: left;
+    }
+    th, td {
+        border: 1px solid #eaeaea;
+        padding: 10px;
+    }
+</style>
